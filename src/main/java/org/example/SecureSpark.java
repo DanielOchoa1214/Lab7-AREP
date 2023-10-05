605a87cb059d1a1e2a4f1721d6f5f6e518405465
DanielOchoa <danochoa1412@gmail.com>
2023-10-05 11:55:02 -0500

    Holi

--- src/main/java/org/example/SecureSpark.java
+++ src/main/java/org/example/SecureSpark.java
@@ -1,2 +1,21 @@
-package org.example;public class SecureSpark {
+package org.example;
+
+import static spark.Spark.*;
+public class SecureSpark {
+    public static void main(String[] args) {
+        port(getPort());
+        secure("keystores/ecikeystore.p12", "123456", null, null);
+        get("/health", (req, res) -> "Wenas");
+    }
+
+    private static int getPort() {
+        if(System.getenv("PORT") != null){
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 35000;
+    }
+
+    private static void readUrl(){
+
+    }
 }
